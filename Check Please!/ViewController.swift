4661a2339c180653b9bb7904bb5b0312f3ea1c47
Mike Lin <mlinchu@ucsd.edu>
2017-12-30 18:30:03 -0800

    Finished UI Layout for iPhone 7, added new feature to spit total bill with up to four parties

--- Check Please!/ViewController.swift
+++ Check Please!/ViewController.swift
@@ -10,16 +10,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var twoWayBillLabel: UILabel!
+    @IBOutlet weak var threeWayBillLabel: UILabel!
+    @IBOutlet weak var fourWayBillLabel: UILabel!
+    
+    
+    var userIsTyping = false
+    var billStored = false
+    let tipPercentages = [0.15, 0.18, 0.2]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.billField.becomeFirstResponder()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if billStored {
+            let defaults = UserDefaults.standard
+            let bill = defaults.double(forKey: "bill")
+            billField.text = String(format: "%.2f", bill)
+        }
+        print("view will appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        let bill = Double(billField.text!) ?? 0
+        let defaults = UserDefaults.standard
+        defaults.set(bill, forKey: "bill")
+        defaults.synchronize()
+        billStored = true
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
 
-
+    
+//    @IBAction func userHasBegunTyping(_ sender: UITextField) {
+//        if !userIsTyping {
+//            billField.text = ""
+//            userIsTyping = true
+//        }
+//    }
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        twoWayBillLabel.text = String(format: "$%.2f", total/2)
+        threeWayBillLabel.text = String(format: "$%.2f", total/3)
+        fourWayBillLabel.text = String(format: "$%.2f", total/4)
+    }
 }
 
